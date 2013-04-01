901c31b2a4bb09ac575aa043ee6a442882ce19ef
andone <412057284@qq.com>
2013-04-01 16:51:26 +0800

    20130401
    change date

--- UnitTest/ViewController.m
+++ UnitTest/ViewController.m
@@ -25,6 +25,7 @@
 
     NSLog(@"change for github");
     NSLog(@"viewDidLoad");
+    NSLog(@"change at 2013-4-1");
 	// Do any additional setup after loading the view, typically from a nib.
     if(self.array){
         NSLog(@"not nil");
